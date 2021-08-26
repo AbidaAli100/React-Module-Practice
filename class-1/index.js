0c9b38bf6482034ae3f424969dde6fe67ca71f5e
AbidaAli100 <abida.ali100@gmail.com>
2021-08-26 06:37:22 +0500

    create a UI using react createElement API

---
+++ class-1/index.js
@@ -0,0 +1,5 @@
+const getElement = document.querySelector('#root')
+const element = document.createElement('div')
+element.textContent = "Hello there, I am Abida  ";
+element.className = 'containor';
+getElement.appendChild(element);
