3dd2c85bda8a876e6fea7bc84a5df210f72e3265
AbidaAli100 <abida.ali100@gmail.com>
2021-08-27 11:54:53 +0500

    rendered two elements side by side

--- class-1/index.js
+++
@@ -1,5 +0,0 @@
-const getElement = document.querySelector('#root')
-const element = document.createElement('div')
-element.textContent = "Hello there, I am Abida  ";
-element.className = 'containor';
-getElement.appendChild(element);
